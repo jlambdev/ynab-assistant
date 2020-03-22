96e34c9ca1007085c1c4a0a72b7033fd45e0efc2
Jonathan Lamb <jlamb.8153@gmail.com>
2020-03-22 11:57:12 +0100

    Update monzo schema

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,34 +1,34 @@
 export const DATE = "Date",
-  DESCRIPTION = "Description",
-  OUTFLOW = "Outflow",
-  INFLOW = "Inflow",
-  AMOUNT = "Amount",
-  SCHEMAS = {
-    n26: {
-      path: "YNAB_n26.csv",
-      headers: [DATE, DESCRIPTION, AMOUNT],
-      columnMap: [0, 1, 6]
-    },
-    lloyds: {
-      path: "YNAB_lloyds.csv",
-      headers: [DATE, DESCRIPTION, OUTFLOW, INFLOW],
-      columnMap: [0, 4, 5, 6]
-    },
-    monzo: {
-      path: "YNAB_monzo.csv",
-      headers: [DATE, DESCRIPTION, AMOUNT],
-      columnMap: [1, 8, 2],
-      trimDate: true
-    },
-    transferwise: {
-      path: "YNAB_transferwise.csv",
-      headers: [DATE, DESCRIPTION, AMOUNT],
-      columnMap: [1, 4, 2]
-    },
-    revolut: {
-      path: "YNAB_revolut.csv",
-      headers: [DATE, DESCRIPTION, OUTFLOW, INFLOW],
-      columnMap: [0, 1, 2, 3],
-      formatDate: true
-    }
-  };
+    DESCRIPTION = "Description",
+    OUTFLOW = "Outflow",
+    INFLOW = "Inflow",
+    AMOUNT = "Amount",
+    SCHEMAS = {
+        n26: {
+            path: "YNAB_n26.csv",
+            headers: [DATE, DESCRIPTION, AMOUNT],
+            columnMap: [0, 1, 6]
+        },
+        lloyds: {
+            path: "YNAB_lloyds.csv",
+            headers: [DATE, DESCRIPTION, OUTFLOW, INFLOW],
+            columnMap: [0, 4, 5, 6]
+        },
+        monzo: {
+            path: "YNAB_monzo.csv",
+            headers: [DATE, DESCRIPTION, AMOUNT],
+            columnMap: [1, 5, 7],
+            trimDate: true
+        },
+        transferwise: {
+            path: "YNAB_transferwise.csv",
+            headers: [DATE, DESCRIPTION, AMOUNT],
+            columnMap: [1, 4, 2]
+        },
+        revolut: {
+            path: "YNAB_revolut.csv",
+            headers: [DATE, DESCRIPTION, OUTFLOW, INFLOW],
+            columnMap: [0, 1, 2, 3],
+            formatDate: true
+        }
+    };
